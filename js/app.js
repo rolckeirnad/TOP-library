4da4a2687f2a65343c869069bd27c30538a21b13
Daniel Gallardo <dgallardo.c@outlook.com>
2022-04-23 13:14:13 -0500

    Add Book constructor and helper functions

---
+++ js/app.js
@@ -0,0 +1,24 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read ? "already read" : "not read yet"}`;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    return;
+}
+
+function displayBooks() {
+    for (let book of myLibrary) {
+        console.log(book.info());
+    }
+}
