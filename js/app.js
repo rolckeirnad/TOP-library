84bcad5cf02aa21eb2cee30f9be5549c0da0ff4d
Daniel Gallardo <dgallardo.c@outlook.com>
2022-04-24 20:46:48 -0500

    Add helper function to create card elements

--- js/app.js
+++ js/app.js
@@ -22,3 +22,55 @@ function displayBooks() {
         console.log(book.info());
     }
 }
+
+function createCard(title, author, content = "", pages, read) {
+    const card = document.createElement('div');
+    card.classList.add("card");
+
+    const figure = document.createElement('figure');
+    figure.classList.add("cardImgContainer");
+    card.appendChild(figure);
+
+    const img = document.createElement('img');
+    img.classList.add("bookImg");
+    img.setAttribute('src', "./img/pexels-heather-mckeen-582070.jpg");
+    img.setAttribute('alt', "book image");
+    figure.appendChild(img);
+
+    const cardData = document.createElement('div');
+    cardData.classList.add('cardData');
+    card.appendChild(cardData);
+
+    const titleSpan = document.createElement('span');
+    titleSpan.classList.add("bookTitle");
+    titleSpan.textContent = title;
+    cardData.append(titleSpan);
+
+    const authorContainer = document.createElement('p');
+    authorContainer.textContent = "by ";
+    const authorName = document.createElement('span');
+    authorName.classList.add("bookAuthor");
+    authorName.textContent = author;
+    authorContainer.appendChild(authorName);
+    cardData.append(authorContainer);
+
+    const bookSummary = document.createElement('p');
+    bookSummary.classList.add('bookSummary');
+    bookSummary.textContent = content;
+    cardData.appendChild(bookSummary);
+    
+    const pagesContainer = document.createElement('p');
+    pagesContainer.textContent = "Pages: ";
+    const pagesNumber = document.createElement('span');
+    pagesNumber.classList.add("bookPages");
+    pagesNumber.textContent = pages;
+    pagesContainer.appendChild(pagesNumber);
+    cardData.appendChild(pagesContainer);
+
+    const toggleButton = document.createElement('button');
+    toggleButton.classList.add("readButton");
+    toggleButton.textContent = read ? "Already read" : "Mark as read" ;
+    cardData.appendChild(toggleButton);
+
+    return card;
+}
